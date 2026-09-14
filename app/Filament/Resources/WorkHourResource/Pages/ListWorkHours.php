d58f129e083b02803ff24f05f4c6ad29f935874c
Marco Xot <marco76tv@hotmail.com>
2026-09-14 16:52:27 +0200

    .

--- app/Filament/Resources/WorkHourResource/Pages/ListWorkHours.php
+++ app/Filament/Resources/WorkHourResource/Pages/ListWorkHours.php
@@ -4,22 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Employee\Filament\Resources\WorkHourResource\Pages;
 
-use Filament\Tables\Columns\Column;
 use Modules\Employee\Filament\Resources\WorkHourResource;
-use Modules\Employee\Filament\Resources\WorkHourResource\Tables\WorkHoursTable;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListWorkHours extends XotBaseListRecords
 {
     protected static string $resource = WorkHourResource::class;
-
-    /**
-     * Definisce le colonne della tabella per la lista delle ore di lavoro.
-     *
-     * @return array<string, Column>
-     */
-    public function getTableColumns(): array
-    {
-        return (new WorkHoursTable())->getTableColumns();
-    }
 }
